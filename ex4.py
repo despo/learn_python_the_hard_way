35f5c3c94f41999ca568a1ae2d4f492185879239
Despo Pentara <despo@despo.me>
2011-08-29 17:20:35 +0100

    Extra credit 3: Added commend above each assignment

--- ex4.py
+++ ex4.py
@@ -1,10 +1,20 @@
+# We have 100 cars
 cars = 100
+# Each car has 4 sits
 space_in_a_car = 4.0
+# There are 30 available drivers
 drivers = 30
+# There are 90 passengers
 passengers = 90
+# The number of not driven cars can be calculated by subtracting the number of drivers from
+# the existing cars
 cars_not_driven = cars - drivers
+# The number of driven cars is equal to the number of existing drivers
 cars_driven = drivers
+# The carpool capacity is equal to the number of driven cars multiplied by the available sits
+# in each car
 carpool_capacity = cars_driven * space_in_a_car
+# The average number of passengers per car equals the number of drivers divided by the driven cars
 average_passengers_per_car = passengers / cars_driven
 
 print "There are", cars, "cars available."
