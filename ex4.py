9f348cad87fe56ab51f12542826f7fad357e282a
Despo Pentara <despo@despo.me>
2011-08-29 17:17:28 +0100

    Extra credit - error explanation

--- ex4.py
+++ ex4.py
@@ -13,3 +13,12 @@ print "There will be", cars_not_driven, "empty cars today."
 print "We can transport", carpool_capacity,"people today."
 print "We have", passengers, "to carpool today."
 print "We need to put about", average_passengers_per_car, "in each car."
+
+# Extra credit error
+# average_passengers_per_car = car_pool_capacity / passenger
+#NameError: name 'car_pool_capacity' is not defined
+
+# Explanation:
+# The variablel car_pool_capacity has not be initialised in the scope of this script
+# so the script does not know how to calculate it
+
