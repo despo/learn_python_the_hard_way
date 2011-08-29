1dbea599ff1cd6f2a84db8f2926c129042c44da7
Despo Pentara <despo@despo.me>
2011-08-29 18:39:23 +0100

    Extra credit 1: Add comments
    How many comments??

--- ex7.py
+++ ex7.py
@@ -1,6 +1,10 @@
+# print the sentence
 print "Mary had a little lamb."
+# print the sentence substrituting %s with snow
 print "Its fleece was white as %s." % 'snow'
+# print the sentence
 print "And everywhere that Mary went."
+# print ten dots
 print "." * 10 # what'd that do ?
 
 end1 = "C"
@@ -16,7 +20,7 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-#watch taht comma at the end. try removing it to see what happens
-print end1 + end2 + end3 + end4 + end5 + end6
+#watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
 
