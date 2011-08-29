cc3315e471835f1bb6abb8d00555a3a639fbcc61
Despo Pentara <despo@despo.me>
2011-08-29 17:55:40 +0100

    Extra credit 3 : Convert inches and poungs to cm an kg

--- ex5.py
+++ ex5.py
@@ -1,14 +1,18 @@
 name = 'Zed A. Shaw'
 age = 35
 height = 74 #inches
+height_in_cm = height * 2.54
 weight = 180 #lbs
+weight_in_kg = weight / 2.2
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
 print "Let's talk about %s." % name
 print "He's %r inches tall." % height
+print "He's %r cm tall." % height_in_cm
 print "He's %r pounds heavy." % weight
+print "He's %r kg heavy." % weight_in_kg
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
