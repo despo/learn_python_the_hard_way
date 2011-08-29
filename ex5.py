a169c198be11780b1a2bb2e5cec236fc59735fd0
Despo Pentara <despo@despo.me>
2011-08-29 17:53:25 +0100

    Extra credit 2 - try %r format

--- ex5.py
+++ ex5.py
@@ -7,8 +7,8 @@ teeth = 'White'
 hair = 'Brown'
 
 print "Let's talk about %s." % name
-print "He's %d inches tall." % height
-print "He's %d pounds heavy." % weight
+print "He's %r inches tall." % height
+print "He's %r pounds heavy." % weight
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
