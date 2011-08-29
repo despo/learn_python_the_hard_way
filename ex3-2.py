0d0963763a99599e35d9bb43b3430ed005820fcd
Despo Pentara <despo@despo.me>
2011-08-29 16:49:16 +0100

    Extra credit: find something you need to calculate

---
+++ ex3-2.py
@@ -0,0 +1,12 @@
+print "I will now count my pencils"
+
+print "Red pencils", 3 + 4 + 30/3
+
+print "Blue pencils", 2 + 7 + 30/3*2
+
+print "Yellow pencils", 1 + 3 - 2 + 40/4
+
+print "All the pencils", 3 + 4 + 30/3 + 2 + 7 + 30/3*2 + 1 + 3 - 2 + 40/4
+
+print "There are more blue than red pencils:", 3 + 4 + 30/3 > 2 + 7 + 30/3*2
+print "There are less yellow than blue pencils", 1 + 3 - 2 + 40/4 < 2 + 7 + 30/3*2
