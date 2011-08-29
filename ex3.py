7c0b716912b742af7cc2ec5098d5b5f1c1ed9d3f
Despo Pentara <despo@despo.me>
2011-08-29 16:42:22 +0100

    Exercise 3 - with comments

---
+++ ex3.py
@@ -0,0 +1,32 @@
+print "I will now count my chickens:"
+
+# Add 30 divided by 6 to 25
+print "Hens", 25 + 30 / 6
+# Subtract 25 multiplied by 3 mod 4 from 100
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+# Add up all the eggs
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Check that 3-2 is les than 5-7
+print 3 - 2 < 5 - 7
+
+# Add 2 to 3
+print "What is 3 + 2?", 3 + 2
+# Subtract 7 from 5
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+# Check if 5 is greater than -2
+print "Is it greater?", 5 > -2
+# Check if 5 is greater or equal than -2
+print "Is it greater or equal?", 5 >= -2
+#Check that 5 is less or equal than -2
+print "Is it less or equal?", 5 <= -2
