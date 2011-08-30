24fa289d7605480f9f870d4621a63e8fdeaf689c
Despo Pentara <despo@extractmethod.com>
2011-08-30 19:10:11 +0100

    Extra credit 8: Close the files

--- ex15.py
+++ ex15.py
@@ -1,4 +1,19 @@
-print "Type a file you'll like to read:"
+# import mofule argv from system
+from sys import argv
+
+# assign the first argument to script and the second to filename
+script, filename = argv
+
+# open the file
+txt = open(filename)
+
+# print the filename
+print "Here's your file %r:" % filename
+# read and print the open file
+print txt.read()
+txt.close()
+
+print "I'll also ask you to type it again:"
 # read from user input the filename
 file_again = raw_input('>')
 
@@ -7,3 +22,4 @@ txt_again = open(file_again)
 
 # read and print the open file
 print txt_again.read()
+txt_again.close()
