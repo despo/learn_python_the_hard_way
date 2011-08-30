ec1d0624269fcb4d0fc64728ea6589d375ef8e4e
Despo Pentara <despo@extractmethod.com>
2011-08-30 19:02:45 +0100

    Extra credit 1: Write above each line what it does

--- ex15.py
+++ ex15.py
@@ -1,15 +1,23 @@
+# import mofule argv from system
 from sys import argv
 
+# assign the first argument to script and the second to filename
 script, filename = argv
 
+# open the file
 txt = open(filename)
 
+# print the filename
 print "Here's your file %r:" % filename
+# read and print the open file
 print txt.read()
 
 print "I'll also ask you to type it again:"
+# read from user input the filename
 file_again = raw_input('>')
 
+# open the filename
 txt_again = open(file_again)
 
+# read and print the open file
 print txt_again.read()
