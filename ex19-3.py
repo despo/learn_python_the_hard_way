89d6fc61f5d5b4624a55c63cd59771d06da4a31b
Despo Pentara <despo@despo.me>
2011-08-31 19:06:41 +0100

    Extra credit 3: Write one more function and run it...

---
+++ ex19-3.py
@@ -0,0 +1,16 @@
+def add(x, y):
+  print "x=%d" % x
+  print "y=%d" % y
+  print "x+y=%d\n" % (x+y)
+
+add(5,5)
+
+x = 10
+y = 20
+add(x,y)
+add(x+5,y)
+add(x,y+5)
+add(y-x,y*2)
+add(x,100)
+add(30,y)
+add(x/y,15)
