67259696bda7e1df1c409f2a2622c2d2c0b36a2e
Despo Pentara <despo@despo.me>
2011-09-04 17:07:32 +0100

    Extra credit 4: Use function defined in ec3

--- exercises/ex33.py
+++ exercises/ex33.py
@@ -14,6 +14,7 @@ def print_numbers():
   for num in numbers:
     print num
 
+# extra credit 2
 numbers = []
 add_numbers_to_list(6)
 print_numbers()
@@ -22,3 +23,11 @@ numbers = []
 add_numbers_to_list(3)
 print_numbers()
 
+# extra credit 4
+numbers = []
+add_numbers_to_list(3, 2)
+print_numbers()
+
+numbers = []
+add_numbers_to_list(15, 3)
+print_numbers()
